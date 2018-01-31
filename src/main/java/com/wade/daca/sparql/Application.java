cbf197975a446af2aa714cae377c422309230c88
Barcan Virgil <ghitza9404@gmail.com>
2018-01-31 23:23:16 +0200

    Tiny little hack

--- src/main/java/com/wade/daca/sparql/Application.java
+++ src/main/java/com/wade/daca/sparql/Application.java
@@ -3,10 +3,18 @@ package com.wade.daca.sparql;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+        ProcessBuilder pb = new ProcessBuilder("java", "-server -Xmx4g -jar blazegraph.jar");
+        try {
+            Process process = pb.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         SpringApplication.run(Application.class, args);
     }
 
